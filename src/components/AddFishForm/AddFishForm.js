274445eadfdb4d1e953e8b7b44dc35a503e5f5cc
femmebot <femmebot@gmail.com>
2018-08-26 00:57:30 -0400

    init inventory form

---
+++ src/components/AddFishForm/AddFishForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class AddFishForm extends Component {
+
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = event => {
+    // Stop form from submitting
+    event.preventDefault();
+    // get values from form
+    // console.log(this.nameRef.value.value);
+    const fish = {
+      name: this.nameRef.value.value,
+      price: parseFloat(this.priceRef.value.value),
+      status: this.statusRef.value.value,
+      desc: this.descRef.value.value,
+      image: this.imageRef.value.value
+    };
+    // create fish object with the form values
+    console.log(fish);
+
+  };
+
+  render() {
+    return (
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name"/>
+        <input name="price" ref={this.priceRef}type="text" placeholder="Price"/>
+        <select name="status" ref={this.statusRef}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold out!</option>
+        </select>
+        <textarea name="desc" ref={this.descRef} placeholder="Desc"/>
+        <input name="image" ref={this.imageRef} type="text" placeholder="Image"/>
+        <button type="submit">+ Add Fish</button>
+      </form>
+    )
+  }
+}
+
+export default AddFishForm;
