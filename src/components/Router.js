9c2721c77e94a0d2ddc1d13df22c2ae4cfbcadf4
Phoebe E <phoebee@Phoebes-MacBook-Air.local>
2018-09-02 23:46:29 -0400

    +not found

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import StorePicker from './StorePicker/StorePicker';
 import App from './App';
+import NotFound from "./NotFound";
+
 
 const Router = (props) => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={ StorePicker } />
       <Route path="/store/:storeID" component={ App } />
+      <Route component={ NotFound } />
     </Switch>
   </BrowserRouter>
 )
